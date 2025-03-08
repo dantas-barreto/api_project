fc2ff759499a5eb855fbfea39d5f64d3364b490b
dantas-barreto <jpsdb10@hotmail.com>
2025-03-08 14:36:29 -0300

    Implementação da camada de domínio

--- src/main/java/com/manjanga/domain/Application.java
+++ src/main/java/com/manjanga/domain/Application.java
@@ -1,4 +1,4 @@
-package com.manjanga.api_project;
+package com.manjanga.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/manjanga/domain/ApiProjectApplicationTests.java
+++ src/test/java/com/manjanga/domain/ApiProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.manjanga.api_project;
+package com.manjanga.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
